b0c9af8ba0f35295df0cd5415cc707e4fdec5755
minyul <ggomjae@gmail.com>
2023-03-13 23:42:53 +0900

    [FEAT] api Kotlin sample 추가

---
+++ api-kt-test/src/main/kotlin/me/pig/minyul/KtApiTest.kt
@@ -0,0 +1,11 @@
+package me.pig.minyul
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KtApiTest
+
+fun main(args: Array<String>) {
+    runApplication<KtApiTest>(*args)
+}
